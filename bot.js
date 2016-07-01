130035396e6bb1cedce3e92c526b1983bac836fe
Harry Engoren <harrison.engoren@gmail.com>
2016-07-01 11:57:11 -0500

    wrk

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" :  "cmon" + name + ", boi yo look like a muhfuckin" + adjectives[randomadj] + animals[randomanimal] +  ". Lookin at me like \n \n" + botResponse + "\n \n wit yo ugly " + bakes[randombake] + " face ass like shit boi"
+    "text" :  "cmon " + name + ", boi yo look like a muhfuckin " + adjectives[randomadj] + " " + animals[randomanimal] +  ". Lookin at me like \n \n" + botResponse + "\n \n wit yo ugly " + bakes[randombake] + " face ass like shit boi"
     //"text" :  r
   };
 
