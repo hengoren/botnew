998f5bf5493eb356bcaddb11652eaac3960e2c63
Harry Engoren <harrison.engoren@gmail.com>
2016-07-01 12:55:12 -0500

    wrk

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ var chunk;
 function respond() {
   // chunk = this.req.chunks[0]
   var request = JSON.parse(this.req.chunks[0]), //length is one
-      botRegex = /^./;
+      botRegex = /july/;
       //botRegex = /^\/cool guy$/;
 
 
