bf51713d608f66403baba491138c2f6ea112fda3
Harry Engoren <harrison.engoren@gmail.com>
2016-07-05 14:05:35 -0500

    undefinederror

--- bot.js
+++ bot.js
@@ -125,7 +125,7 @@ function respond() {
   // if(request.text && botRegex.test(request.text) && userId != '345971"') {  //&& userId != '345971"' 2100646 //&& userId =='345971"'
   //if (userId != '345971"') {
   //if (sender_type != bot) {
-    if(request.text && botRegex.test(request.text)) {
+    if(request.text && botRegex.test(request.text) && request.sender_type != bot) {
     toSend = request.text;
     this.res.writeHead(200);
     postMessage();
