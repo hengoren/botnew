3a206ed2edd471df5442b06f6bd85147b70c5ef1
Harry Engoren <harrison.engoren@gmail.com>
2016-07-05 11:37:34 -0500

    undefinederror

--- bot.js
+++ bot.js
@@ -158,7 +158,7 @@ function postMessage() {
     "bot_id" : botID,
     "text" :  "cmon " + name + ", boi yo look like a muhfuckin " + adjectives[randomadj] + " " + animals[randomanimal] +  ". Lookin at me like \n \n" + botResponse + "\n \n wit yo ugly " + bakes[randombake] + " face ass like shit boi"
     //"text" :  r + "\n attatchments: " + attatchments + "\n avatar_url: " + avatar_url + "\n created_at: " + created_at + "\n group_id: " + group_id + "\n id: " + id + 
-              "\n sender_id: " + sender_id + "\n source_guid: " + source_guid + "\n system: " + system + "\n text: " + text + "\n user_id: " + user_id;
+              //"\n sender_id: " + sender_id + "\n source_guid: " + source_guid + "\n system: " + system + "\n text: " + text + "\n user_id: " + user_id;
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
