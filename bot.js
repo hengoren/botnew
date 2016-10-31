131406fefec4120771f7e943759a08d2d0c3596e
Harry Engoren <harrison.engoren@gmail.com>
2016-10-31 11:41:10 -0400

    cleverbottrialwithfunc

--- bot.js
+++ bot.js
@@ -228,9 +228,9 @@ function postMessage() {
 
   if (infrequent > 0) {
     bot.ask(text, function (err, response) {
-      console.log(response)
-      //infrequentresponse = response;
-      //postToGroup(infrequentresponse);
+      //console.log(response)
+      infrequentresponse = response;
+      postToGroup(infrequentresponse);
     });
   }
 
@@ -246,41 +246,41 @@ function postMessage() {
 
 
   //put all this in a function called postToGroup 
-  botResponse = cool();
+  funciton postToGroup() {
+    botResponse = cool();
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+    options = {
+      hostname: 'api.groupme.com',
+      path: '/v3/bots/post',
+      method: 'POST'
+    };
 
-  body = {
-   "bot_id" : botID,
-   "text" : infrequentresponse
-   //"text" : randostring
-   //"text" : "cmon " + name + ", boi yo look like a muhfuckin " + adjectives[randomadj] + " " + animals[randomanimal] +  ". Lookin at me like \n \n" + botResponse + "\n \n wit yo ugly " + bakes[randombake] + " face ass like shit boi"
-   //"text" : r //+ "\n attatchments: " + attatchments + "\n avatar_url: " + avatar_url + "\n created_at: " + created_at + "\n group_id: " + group_id + "\n id: " + id + 
-             //"\n sender_id: " + sender_id + "\n source_guid: " + source_guid + "\n system: " + system + "\n text: " + text + "\n user_id: " + user_id;
- };
+    body = {
+     "bot_id" : botID,
+     "text" : infrequentresponse
+     //"text" : randostring
+     //"text" : "cmon " + name + ", boi yo look like a muhfuckin " + adjectives[randomadj] + " " + animals[randomanimal] +  ". Lookin at me like \n \n" + botResponse + "\n \n wit yo ugly " + bakes[randombake] + " face ass like shit boi"
+     //"text" : r //+ "\n attatchments: " + attatchments + "\n avatar_url: " + avatar_url + "\n created_at: " + created_at + "\n group_id: " + group_id + "\n id: " + id + 
+               //"\n sender_id: " + sender_id + "\n source_guid: " + source_guid + "\n system: " + system + "\n text: " + text + "\n user_id: " + user_id;
+   };
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+    console.log('sending ' + botResponse + ' to ' + botID);
 
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
+    botReq = HTTPS.request(options, function(res) {
+        if(res.statusCode == 202) {
+          //neat
+        } else {
+          console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
+    botReq.on('error', function(err) {
+      console.log('error posting message '  + JSON.stringify(err));
+    });
+    botReq.on('timeout', function(err) {
+      console.log('timeout posting message '  + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
+  }
 
-
-exports.respond = respond;
+  exports.respond = respond;
