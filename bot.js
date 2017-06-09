b4cc9ce47f3b99439111c03dfe71ee8df9027aa3
Harrison Engoren <harrison.engoren@gmail.com>
2017-06-09 16:24:06 -0400

    Cleverbot Api Call with a simple HttpClient to make the GET request

--- bot.js
+++ bot.js
@@ -201,15 +201,15 @@ function respond() {
 
 
 /*API code*/
-var HttpClient = function(){
+var HttpClient = function() {
   this.get = function(aUrl, aCallback) {
     var anHttpRequest = new XMLHttpRequest();
     anHttpRequest.onreadystatechange = function() {
-      if (anHttpRequest.readystate == 4 && anHttpRequest.status == 200)
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
         aCallback(anHttpRequest.responseText);
     }
 
-    anHttpRequest.open( "GET", aUrl, true);
+    anHttpRequest.open("GET", aUrl, true);
     anHttpRequest.send(null);
   }
 }
