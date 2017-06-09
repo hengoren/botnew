183ca060c50da0e93f7e47ffcd6e561ba5d4e0b5
Harrison Engoren <harrison.engoren@gmail.com>
2017-06-09 09:07:15 -0400

    mocking response

--- bot.js
+++ bot.js
@@ -199,6 +199,7 @@ function spongebobMock(text) {
   randomint = Math.floor(Math.random() * 2)
   newtext = ''
   for (i = 0; i < text.length; i++) {
+    newtext += str(randomint)
     if (randomint == 0) {
       newtext += text.charAt(i).toUpperCase() 
     }
