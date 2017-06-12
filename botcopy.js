5a9e0a56e4d1e2497e7b4268014fce88548e2f8a
Harrison Engoren <harrison.engoren@gmail.com>
2017-06-12 09:18:08 -0400

    debugging cleverbot api call

--- botcopy.js
+++ botcopy.js
@@ -83,9 +83,7 @@ function generateCleverbotResponse(input_text) {
 		console.log('error:', error); // Prints the error if one occurred
 		console.log('statusCode:', response && response.statusCode); // Print the response status code if response is returned
 		console.log('body:', body); //print the stuff
-		var b = JSON.stringify(body)
-		toreturn = b.output
-		console.log("the b ", b)
+		toreturn = JSON.parse(body).output;
 		console.log("TORETURN!!!!! ", toreturn)
 		
 	});
